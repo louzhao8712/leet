8e696d0fe6775f14f0efa775b6ec8862fbff873c
louzhao8712 <louzhao8712@gmail.com>
2016-04-06 13:01:44 -0700

    Adding pure dict method trie

--- concept_implementation/trie.py
+++ concept_implementation/trie.py
@@ -1,3 +1,12 @@
+class TrieNode(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.childs = {}
+        self.isWord = False
+
+
 class Trie(object):
 
     def __init__(self):
@@ -15,7 +24,7 @@ class Trie(object):
                 child = TrieNode()
                 p.childs[x] = child
             p = p.childs[x]
-        p.isWord = True        
+        p.isWord = True
 
     def search(self, word):
         """
@@ -32,15 +41,15 @@ class Trie(object):
 
     def delete(self, word):
         node = self.root
-        queue = []
+        queue = [] #this is actually a stack
         for letter in word:
             queue.append((letter, node))
             child = node.childs.get(letter)
             if child is None:
                 return False
             node = child
-        if not node.isWord:
-            return False
+        # no such word in the trie
+        if not node.isWord:   return False
         if len(node.childs): # this path has other owrd
             node.isWord = False
         else:
@@ -52,3 +61,29 @@ class Trie(object):
                 if len(node.childs) or node.isWord:
                     break
         return True
+        
+#====dict method====================================
+def make_trie(*words):
+    root = dict()
+    for word in words:
+        current_dict = root
+        for letter in word:
+            #setdefault is similar to get(),
+            #but will set dict[key]=default if key is not already in dict
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict['_end_'] = '_end_'
+    return root
+
+trie = make_trie('foo', 'bar', 'baz', 'barz')
+print trie
+
+def in_trie(word, trie):
+    current_dict = trie
+    for letter in word:
+        if letter not in current_dict:
+            return False
+        else:
+            current_dict = current_dict[letter]
+    return '_end_' in current_dict
+
+print in_trie('ba', trie)
