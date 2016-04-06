3b8b7d026c520c0b47169f81e7632b38af906b49
louzhao8712 <louzhao8712@gmail.com>
2016-04-05 23:53:11 -0700

    first file

---
+++ 1_two_sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+		tb={}
+		for i in xrange(len(nums)):
+			tmp = target = nums[i]
+			if tmp in tb:
+				return [tb[tmp],i]
+			else:
+				tb[nums[i]]=i
+			
+	
