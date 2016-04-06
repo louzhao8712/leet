2b0df13e446c2e84d47e72e27cd1d078013b26b8
louzhao8712 <louzhao8712@gmail.com>
2016-04-06 00:37:25 -0700

    Update 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-	# add one cmd
-	tb={}
-	for i in xrange(len(nums)):
-		tmp = target = nums[i]
-		if tmp in tb:
-			return [tb[tmp],i]
-		else:
-			tb[nums[i]]=i
+    	# add one cmd
+    	tb={}
+    	for i in xrange(len(nums)):
+    		tmp = target = nums[i]
+    		if tmp in tb:
+    			return [tb[tmp],i]
+    		else:
+    			tb[nums[i]]=i
 			
 	
