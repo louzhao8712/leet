32cba1707ce437f687f6d6e2c191251619586ee2
louzhao8712 <louzhao8712@gmail.com>
2016-04-25 14:21:58 -0700

    0425

--- leet_answer/1forAll.py
+++ leet_answer/1forAll.py
@@ -90,6 +90,226 @@ class Solution3(object):
         currlen = end - start 
         ret = max(ret,currlen)
         return ret
+#---------binary search, divide and con--------------------------
+# Median of Two Sorted Arrays
+class Solution4(object):
+"""
+There are two sorted arrays nums1 and nums2 of size m and n respectively. 
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+"""
+    def helper(self,A,B,K):
+        # index 0 system, kth item is array[k-1]
+        lenA = len(A); lenB = len(B)
+        if lenA > lenB : return self.helper(B,A,K)
+        if len(A) == 0: return B[K-1]
+        if K  ==1 : return min(A[0],B[0])
+        pa= min(K/2,lenA)
+        pb = K-pa
+        if A[pa-1] < B[pb-1]:
+            return self.helper(A[pa:],B,pb)
+        else:
+            return self.helper(A,B[pb:],pa)
+
+    def findMedianSortedArrays(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: float
+        """
+        lenA = len(A); lenB = len(B)
+        if lenA == 0 and lenB == 0: return False
+        if (lenA + lenB)%2 ==1:
+            return self.helper(A,B,(lenA+lenB)/2 +1)
+        else:
+            return (self.helper(A,B,(lenA+lenB)/2 +1) + self.helper(A,B,(lenA+lenB)/2))*0.5
+#-----------------------------------
+#5. Longest Palindromic Substring
+class Solution5(object):
+"""
+Given a string S, find the longest palindromic substring in S.
+You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic subs
+"""
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return self.sol3(s)
+        
+    def sol3(self,s):
+        #Manacher's Algorithm
+        T = self.process(s)
+        C=R=0 #center and right boundary
+        P=[0]*len(T) # counter for each position
+        for i in xrange(1,len(T)-1):
+            i_mirror = 2*C-i
+            if R>i:
+                P[i] = min(R-i,P[i_mirror])
+            else:
+                P[i] = 0
+                
+            while T[i+1+P[i]] ==  T[i-1-P[i]]: P[i] = P[i]+1
+            if i+P[i] > R:
+                C = i
+                R = i+P[i]
+                
+        maxLen = 0
+        centerIndex = 0
+        for i in range(1, len(T)-1):
+            if P[i] > maxLen:
+                maxLen = P[i]
+                centerIndex = i
+        return s[(centerIndex-1-maxLen)/2: (centerIndex-1-maxLen)/2+maxLen]
+        
+        
+    def process(self,s):
+        """
+        ^ head $tail #seperator
+        """
+        ret = "^"
+        for i in xrange(len(s)):
+            ret+="#"+s[i]
+        ret += "#$"
+        return ret
+        
+
+    def dp(self,s):
+        # time o(n^2), space o(n^2)
+        # d[i][j] == True means s[i] to s[j] is a palindrome 
+        n = len(s)
+        longbegin = 0
+        maxlen =1
+        d = [[False for j in xrange(n)] for i in xrange(n)]
+        for i in xrange(n): d[i][i] = True
+        for i in xrange(n-1):
+            if s[i] == s[i+1]: 
+                d[i][i+1] = True
+                longbegin = i
+                maxlen = 2
+        for l in xrange(3,n+1):  #l the length of palindrome string
+            for i in xrange(n-l):
+                j = i+l
+                if s[i]==s[j] and d[i+1][j-1]:
+                    d[i][j]=True
+                    longbegin = i
+                    maxlen = l
+        return s[longbegin:longbegin+maxlen]
+        
+    def sol1(self,s):
+        # solution1 O(n**2) and o(1) space
+        # imporvement of brute force
+        ret = ''
+        for i in xrange(len(s)):
+            # s1 and s2 because the palindrome string center can be i or bewtween i and i+1
+            s1 = self.getlps(s,i,i)
+            if len(s1) > len(ret) : ret = s1
+            s2 = self.getlps(s,i,i+1)
+            if len(s2) > len(ret) : ret = s2
+        return ret
+        
+        
+    def getlps(self,s,l,r):
+        while l >=0  and r < len(s) and s[l] == s[r]:
+            l -=1
+            r +=1
+        return s[l+1:r]
+#-----------------------------------
+#6. ZigZag Conversion
+class Solution6(object):
+"""
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this
+"""
+        if n < 1: return None
+        if n ==1 or n >= len(s): return s
+        return self.sol2(s,n)
+
+        
+    def sol2(self,s,n):
+        arr = ["" for i in xrange(n)]
+        row =0;step =1
+        for c in s:
+            arr[row] += c
+            if row == 0: step =1
+            elif row == n-1: step = -1
+            row += step
+        return "".join(arr)
+        
+        
+    def sol1(self,s,n):
+        # keypoint is r = 2*n-2
+        # arr store string in each level
+        arr = ["" for i in xrange(n)]
+        r = 2*n-2
+        for i in xrange(len(s)):
+            tmp = i%r
+            if tmp < n:
+                arr[tmp] += s[i]
+            else:
+                arr[r-tmp] += s[i]
+        return "".join(arr)
+#-----------------------------------
+#7. Reverse Integer
+class Solution7(object):
+    """
+    Reverse digits of an integer.
+    """
+        #  INT_MAX = 2147483647
+        #  INT_MIN = -2147483648
+        intmax = 2**31-1
+        intmin = -2**31
+        maxd10 = intmax/10
+        ret = 0
+        sign = 1 if x>0 else -1
+        x = abs(x)
+        while x > 0:
+            digit = x%10
+            if ret > maxd10 or ret == maxd10 and digit >7: return 0
+            ret = ret*10 + digit
+            x /=10
+        ret = sign * ret
+        return ret
+#-----------------------------------
+#8. String to Integer (atoi)
+class Solution8(object):
+"""
+Implement atoi to convert a string to an integer.
+
+Hint: Carefully consider all possible input cases.
+If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+
+Notes: It is intended for this problem to be specified vaguely (ie, no given input specs).
+You are responsible to gather all the input requirements up front. 
+"""
+    # note this solition did not mention that we can have 'e' in the string
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        #INT_MAX = 2147483647
+        #INT_MIN = -2147483648
+        str = str.strip()
+        N = len(str)
+        if N == 0:  return 0
+        sign = 1
+        res = 0
+        imin, imax = -1<<31, (1<<31)-1
+        maxd10 = imax/10
+        for i, bit in enumerate(str):
+            if i == 0 and bit in ['-', '+']:
+                if bit == '-':
+                    sign = -1
+            elif bit.isdigit():
+                if res > maxd10 or res == maxd10 and int(bit) >7:
+                    return imax if sign ==1 else imin
+                res = res*10 + int(bit)
+            else:
+                break
+        return sign * res
+#-----------------------------------
+#-----------------------------------
+#-----------------------------------
+#-----------------------------------
 #-----------------------------------
 #-----------------------------------
 #-----------------------------------
