4816c4f2bbc8772a67c64b4fd6160ab97d761687
louzhao8712 <louzhao8712@gmail.com>
2016-05-17 22:14:25 -0700

    0517

--- leet_answer/1forAllv2.py
+++ leet_answer/1forAllv2.py
@@ -3244,3 +3244,25 @@ The relative order inside both the even and odd groups should remain as it was i
 The first node is considered odd, the second node even and so on ... 
 
 """
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None: return head
+        odd = oddHead = head
+        even = evenHead = head.next
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenHead
+        return oddHead
