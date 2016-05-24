275a44e645718131f244bd7008e2a62025b9fe43
louzhao8712 <louzhao8712@gmail.com>
2016-05-23 23:14:22 -0700

    05233

--- leet_answer/1forAllv2.py
+++ leet_answer/1forAllv2.py
@@ -3604,3 +3604,31 @@ class Solution(object):
         return solve("JFK")        
 #-----------------------------------
 #-----------------------------------
+#334. Increasing Triplet Subsequence
+"""
+ Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+
+Formally the function should:
+
+    Return true if there exists i, j, k
+    such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false. 
+
+Your algorithm should run in O(n) time complexity and O(1) space complexity. 
+"""
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # a is the min element
+        # b is the second min close to a
+        a,b = None,None
+        for i in xrange(len(nums)):
+            if a is None or nums[i] <= a:
+                a = nums[i]
+            elif b is None or nums[i] <=b:
+                b = nums[i]
+            else:
+                return True
+        return False
