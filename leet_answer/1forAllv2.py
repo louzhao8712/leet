c4a048fd61057270b4106170d85b1e11e8a13485
louzhao8712 <louzhao8712@gmail.com>
2016-06-09 23:00:33 -0700

    0609

--- leet_answer/1forAllv2.py
+++ leet_answer/1forAllv2.py
@@ -1748,6 +1748,27 @@ class Solution(object):
         return dp[0]-1
 #-----------------------------------
 #-----------------------------------
+#136. Single Number
+"""
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? 
+"""
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ln = len(nums)
+        if ln == 0: return None
+        res = nums[0]
+        if ln == 1: return res
+        for i in xrange(1,ln):
+            res  ^= nums[i]
+        return res
+#-----------------------------------
 #-----------------------------------
 #-----------------------------------
 #139. Word Break
@@ -3284,6 +3305,58 @@ class Codec:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
 #-----------------------------------
+#-----------------------------------
+#299. Bulls and Cows
+"""
+ou are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+
+For example:
+
+Secret number:  "1807"
+Friend's guess: "7810"
+
+Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)
+
+Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
+
+Please note that both secret number and friend's guess may contain duplicate digits, for example:
+
+Secret number:  "1123"
+Friend's guess: "0111"
+
+In this case, the 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow, and your function should return "1A1B".
+
+You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+"""
+class Solution(object):
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+        A = 0
+        B = 0
+        n = len(secret)
+        secret = [int(x) for x in secret]
+        guess = [int(x) for x in guess]
+        count = [0 for i in xrange(10)] # since only 0-9 ten numbers
+        for i in xrange(n):
+            count[secret[i]] +=1
+            count[guess[i]] -=1
+        # count[i] > 0 means for number i, more in secret then in guess
+        for i in xrange(n):
+            g = guess[i]
+            s = secret[i]
+            if g == s : A+=1
+            else:
+                if count[g] > 0: #there are extra g in secret
+                    B+=1
+                if count[s] <0: #there are extra s in guess
+                    B+=1
+                count[g] -=1
+                count[s]+=1
+        return str(A)+'A'+str(B)+'B'
 #-----------------------------------
 #300. Longest Increasing Subsequence
 """
