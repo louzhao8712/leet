e87ceac0162cc5d0b1582d62895c815ea15e6bbf
louzhao8712 <louzhao8712@gmail.com>
2016-07-08 13:29:52 -0700

    0708

--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -3229,6 +3229,37 @@ class Solution(object):
         queue.append(i*self.col + j)
 #-----------------------------------
 #-----------------------------------
+#202. Happy Number
+"""
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+    12 + 92 = 82
+    82 + 22 = 68
+    62 + 82 = 100
+    12 + 02 + 02 = 1
+"""
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        l= []
+        while True:
+            sum =0
+            while n:
+                digit = n%10
+                sum += digit**2
+                n = n/10
+            n = sum
+            if n== 1 or (n in l): break
+            l.append(n)
+        return n==1
+#-----------------------------------
 #206 Reverse Linked List
 # Definition for singly-linked list.
 # class ListNode(object):
