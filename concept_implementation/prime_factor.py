26ff7652daf7288b2c157fb4fb24ae5a3a22948b
louzhao8712 <louzhao8712@gmail.com>
2016-08-06 01:06:38 -0700

    0806

---
+++ concept_implementation/prime_factor.py
@@ -0,0 +1,22 @@
+def primes(n):
+    primfac = []
+    d = 2
+    while d*d <= n:
+        while (n % d) == 0:
+            primfac.append(d)  # supposing you want multiple factors repeated
+            n //= d
+        d += 1
+    if n > 1:
+       primfac.append(n)
+    return primfac
+
+#------calculate gcd-------------
+def gcd(a, b):
+    """Calculate the Greatest Common Divisor of a and b.
+
+    Unless b==0, the result will have the same sign as b (so that when
+    b is divided by it, the result comes out positive).
+    """
+    while b:
+        a, b = b, a%b
+    return a
