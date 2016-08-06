3334c485cbd052ec028e28998dbe6947bc7d9167
louzhao8712 <louzhao8712@gmail.com>
2016-08-06 10:30:13 -0700

    0806

---
+++ concept_implementation/sort.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+#---------merge sort------------------------
+def msort3(x):
+    result = []
+    if len(x) < 2:
+        return x
+    mid = int(len(x)/2)
+    y = msort3(x[:mid])
+    z = msort3(x[mid:])
+    i = 0
+    j = 0
+    while i < len(y) and j < len(z):
+            if y[i] > z[j]:
+                result.append(z[j])
+                j += 1
+            else:
+                result.append(y[i])
+                i += 1
+    result += y[i:]
+    result += z[j:]
+    return result
+
+# quick sort
+# http://algorithm.yuanbin.me/zh-hans/basics_sorting/quick_sort.html
+def qsort1(alist):
+    #extra place
+    print(alist)
+    if len(alist) <= 1:
+        return alist
+    else:
+        pivot = alist[0]
+        return qsort1([x for x in alist[1:] if x < pivot]) + \
+               [pivot] + \
+               qsort1([x for x in alist[1:] if x >= pivot])
+
+def qsort2(alist):
+    #in place
+    print(alist)
+    if l >= u:
+        return
+
+    m = l
+    for i in xrange(l + 1, u + 1):
+        if alist[i] < alist[l]:
+            m += 1
+            alist[m], alist[i] = alist[i], alist[m]
+    # swap between m and l after partition, important!
+    alist[m], alist[l] = alist[l], alist[m]
+    qsort2(alist, l, m - 1)
+    qsort2(alist, m + 1, u)
+
+unsortedArray = [6, 5, 3, 1, 8, 7, 2, 4]
+print(qsort2(unsortedArray, 0, len(unsortedArray) - 1))
