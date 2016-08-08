d9d5a7e2c1306ba8fcd187c2d3cf29d0df5f2861
louzhao8712 <louzhao8712@gmail.com>
2016-08-08 15:54:08 -0700

    0808

--- concept_implementation/DFSorBFSaGraph.py
+++ concept_implementation/DFSorBFSaGraph.py
@@ -76,4 +76,27 @@ def bfs(graph, start):
     return visited
 
 bfs(graph, 'A') # {'B', 'C', 'A', 'F', 'D', 'E'}
- 
+# Paths
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+list(bfs_paths(graph, 'A', 'F')) # [['A', 'C', 'F'], ['A', 'B', 'E', 'F']
+
+"""
+Knowing that the shortest path will be returned first from the BFS path generator method we can create a useful method which simply returns the shortest path found or ‘None’ if no path exists. 
+"""
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+shortest_path(graph, 'A', 'F') # ['A', 'C', 'F']
--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -2644,7 +2644,71 @@ class Solution(object):
             self.dfs(root.left,level+1)
             self.dfs(root.right,level+1)
 #-----------------------------------
+#108. Convert Sorted Array to Binary Search Tre
+"""
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        return self.dfs(nums,0,len(nums)-1)
+        
+    def dfs(self,nums,start,end):
+        if start > end : return None
+        mid = start + (end-start)/2
+        root = TreeNode(nums[mid])
+        root.left = self.dfs(nums,start,mid-1)
+        root.right = self.dfs(nums,mid +1 ,end)
+        return root
 #-----------------------------------
+#109. Convert Sorted List to Binary Search Tree
+"""
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# trick: convert to array first
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        nums = []
+        curr = head
+        while curr:
+            nums.append(curr.val)
+            curr = curr.next
+        return self.dfs(nums,0,len(nums)-1)
+        
+    def dfs(self,nums,start,end):
+        if start > end : return None
+        mid = (start + end)/2
+        root = TreeNode(nums[mid])
+        root.left = self.dfs(nums,start,mid-1)
+        root.right = self.dfs(nums,mid +1 ,end)
+        return root 
 #-----------------------------------
 #-----------------------------------
 #-----------------------------------
@@ -2653,6 +2717,29 @@ class Solution(object):
 #114 Flatten Binary Tree to Linked List
 """
 Given a binary tree, flatten it to a linked list in-place. 
+ For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+
+The flattened tree should look like:
+
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+
 """
 # Definition for a binary tree node.
 # class TreeNode(object):
