f15385dec65538d1eeca6a585059872af1c8cd65
pearpan <louzhao8712@gmail.com>
2016-09-04 22:20:00 -0700

    0904

--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -2643,6 +2643,37 @@ class Solution(object):
         return maxArea
 
 #-----------------------------------
+#88. Merge Sorted Array
+"""
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+"""
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        # store nums1 from the end!!!
+        end = m+n-1
+        p1 = m-1
+        p2 = n-1
+        while p2 >=0 :
+            if  p1<0 or nums1[p1] < nums2[p2]:
+                nums1[end] = nums2[p2]
+                p2 -=1
+            else:
+                nums1[end] = nums1[p1]
+                p1 -=1
+            end -= 1
+        
+
+#-----------------------------------
 #90. Subsets II
 """
  Given a collection of integers that might contain duplicates, nums, return all possible subsets.
