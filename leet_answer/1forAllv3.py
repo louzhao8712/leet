255adc7a576b8d63c87771cb501f7eb65c019541
louzhao8712 <louzhao8712@gmail.com>
2016-09-22 19:58:20 -0700

    09223

--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -11118,6 +11118,56 @@ class Solution(object):
                 push(i+1,0)
         return ans
 #-----------------------------------
+#374. Guess Number Higher or Lower
+"""
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+
+Example:
+
+n = 10, I pick 6.
+
+Return 6.
+
+"""
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lo  = 1 
+        hi = n
+        if n < 0: return False
+        if n == 1: return 1
+        
+        while lo +1 < hi:
+            mid = (lo+hi)/2
+            res = guess(mid)
+            if  res == 0: return mid
+            elif res == 1: #mid is lower
+                lo = mid +1
+            elif res == -1:
+                hi = mid
+        
+        if guess(lo)==0: return lo
+        if guess(hi)==0: return hi
+        
+#-----------------------------------
 #377. Combination Sum IV
 """
  Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
