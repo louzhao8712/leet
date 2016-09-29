7b3bb51cf6ada2bc13df13fbbe197a1a2e83caa9
louzhao8712 <louzhao8712@gmail.com>
2016-09-29 00:46:07 -0700

    0929

--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -11689,6 +11689,130 @@ class Solution(object):
         return rst
 
 #-----------------------------------
+#355. Design Twitter
+"""
+Design a simplified version of Twitter where users can post tweets, follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. Your design should support the following methods:
+
+    postTweet(userId, tweetId): Compose a new tweet.
+    getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+    follow(followerId, followeeId): Follower follows a followee.
+    unfollow(followerId, followeeId): Follower unfollows a followee.
+
+Example:
+
+Twitter twitter = new Twitter();
+
+// User 1 posts a new tweet (id = 5).
+twitter.postTweet(1, 5);
+
+// User 1's news feed should return a list with 1 tweet id -> [5].
+twitter.getNewsFeed(1);
+
+// User 1 follows user 2.
+twitter.follow(1, 2);
+
+// User 2 posts a new tweet (id = 6).
+twitter.postTweet(2, 6);
+
+// User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+// Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.getNewsFeed(1);
+
+// User 1 unfollows user 2.
+twitter.unfollow(1, 2);
+
+// User 1's news feed should return a list with 1 tweet id -> [5],
+// since user 1 is no longer following user 2.
+twitter.getNewsFeed(1);
+
+"""
+class Twitter(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        #https://discuss.leetcode.com/topic/47838/python-solution
+        #https://discuss.leetcode.com/topic/48166/simple-and-clean-python-code-o-logk-for-getting-news-feed
+        #use heap to merge and sort tweets
+        # use set to store followee list for each user
+        self.time = 0
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followee = collections.defaultdict(set) # A follow B  
+        # a set of people A has followed, i.e self.followees[A].add(B)
+        
+        #time complexity o(lg(k)). K is the number of followee of user
+        #We have O(log(K)) runtime for getting news feed because we do maximum 10 extractions in a heap that holds maximum K elements (similar to what is done in merge K linked lists). The other ops are obviously O(1).
+
+    def postTweet(self, userId, tweetId):
+        """
+        Compose a new tweet.
+        :type userId: int
+        :type tweetId: int
+        :rtype: void
+        """
+        self.time += 1
+        self.tweets[userId].append((-self.time,tweetId)) #For max heap, the top one is the latest time tweet
+
+
+    def getNewsFeed(self, userId):
+        """
+        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+        :type userId: int
+        :rtype: List[int]
+        """
+        h, tweets = [], self.tweets
+        people = self.followee.get(userId, set()) | set([userId]) # two set can be concatenated via '|'
+        #build the heap, sort by time
+        #only push the latest of each person to the heap first
+        for person in people:
+            if person in tweets and tweets[person]:
+                time, tweet = tweets[person][-1] #the latest tweets
+                h.append((time, tweet, person, len(tweets[person]) - 1)) 
+                #len(tweets[person]) - 1, how many tweets left in tweets[person]
+        heapq.heapify(h)
+        news = []
+        #idx is used to prevent when tweets[person] is empty, skip this person
+        for _ in range(10):
+            if h:
+                time, tweet, person, idx = heapq.heappop(h)
+                news.append(tweet)
+                if idx:
+                    #push the next new tweet of the processed person to heap
+                    new_time, new_tweet = tweets[person][idx-1]
+                    heapq.heappush(h, (new_time, new_tweet, person, idx - 1))
+        return news        
+        
+
+    def follow(self, followerId, followeeId):
+        """
+        Follower follows a followee. If the operation is invalid, it should be a no-op.
+        :type followerId: int
+        :type followeeId: int
+        :rtype: void
+        """
+        self.followee[followerId] = self.followee.get(followerId, set()) | set([followeeId])
+        
+
+    def unfollow(self, followerId, followeeId):
+        """
+        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+        :type followerId: int
+        :type followeeId: int
+        :rtype: void
+        """
+        if followerId in self.followee:
+            self.followee[followerId].discard(followeeId)
+            #discard will not throw error if not exist, remove will throw error
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
+#-----------------------------------
 #357. Count Numbers with Unique Digits
 """
 Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
