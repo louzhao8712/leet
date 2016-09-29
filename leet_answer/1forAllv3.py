fcc48cd30d8af798b84f9b6f4cc4a31874eee282
louzhao8712 <louzhao8712@gmail.com>
2016-09-28 19:04:08 -0700

    09283

--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -12265,6 +12265,38 @@ class Solution(object):
                     dp[x+y] += dp[x]
         return dp[target]
 #-----------------------------------
+#383. Ransom Note
+"""
+Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters from  all  the  magazines,  write  a  function  that  will  return  true  if  the  ransom   note  can  be  constructed  from  the  magazines ;  otherwise,  it  will  return  false.   
+
+Each  letter  in  the  magazine  string  can  only  be  used  once  in  your  ransom  note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+"""
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        db= {}
+        for item in magazine:
+            if item not in db: db[item] =1
+            else: db[item] += 1
+        
+        for item in ransomNote:
+            if item not in db: return False
+            elif db[item] <=0 : return False
+            db[item] -=1
+        return True
+#-----------------------------------
 #384. Shuffle an Array
 """
 Shuffle a set of numbers without duplicates.
