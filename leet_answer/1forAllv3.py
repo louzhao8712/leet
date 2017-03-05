e7dd77e0f1550c4b204e5a50d24145a1bdce46c7
louzhao8712 <louzhao8712@gmail.com>
2017-03-05 10:57:10 -0800

    0305

--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -15051,4 +15051,38 @@ class Solution(object):
                 top = stack.pop()
                 root = top.right
         return ret
-            
+
+#-----------------------------------
+#434 Number of Segments in a String
+"""
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+"""
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return self.sol2(s)
+        
+    def sol2(self,s):
+        ret = 0
+        currStr = ""
+        for x in s:
+            if x == " ":
+                if currStr!="": 
+                    ret+=1
+                    currStr =""
+            else:
+                currStr += x
+        if currStr: ret+=1
+        return ret
+    def sol1(self,s):
+        return len(s.split())
