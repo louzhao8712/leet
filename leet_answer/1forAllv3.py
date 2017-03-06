0f658a30280cb9d171ccec06a644e71aadb2983c
Zhao Lou <louzhao@lab126.com>
2017-03-06 00:12:54 -0800

    0306

--- leet_answer/1forAllv3.py
+++ leet_answer/1forAllv3.py
@@ -15164,3 +15164,63 @@ class Solution(object):
         for key in keys:
             ret += db[key]*key
         return ret
+#-----------------------------------
+#463. Island Perimeter
+"""
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+Example:
+
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Answer: 16
+Explanation: The perimeter is the 16 yellow stripes in the image below:
+
+Subscribe to see which companies asked this question.
+"""
+#
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        """
+        loop over the matrix and count the number of islands;
+        if the current dot is an island, count if it has any right neighbour or down neighbour;
+        the result is islands * 4 - neighbours * 2
+        """
+        lrow = len(grid)
+        if lrow == 0: return 0
+        lcol = len(grid[0])
+        if lcol == 0: return 0
+        islands = 0
+        neighbors = 0
+        for i in xrange(lrow):
+            for j in xrange(lcol):
+                if grid[i][j]!=0:
+                    islands +=1
+                    if i+1 <= lrow-1 and grid[i+1][j]: neighbors +=1
+                    if j+1 <= lcol-1 and grid[i][j+1]: neighbors +=1
+                    
+        return 4*islands -2*neighbors       
+        
+    def sol1(self,grid):
+        lrow = len(grid)
+        if lrow == 0: return 0
+        lcol = len(grid[0])
+        if lcol == 0: return 0
+        ret = 0
+        for i in xrange(lrow):
+            for j in xrange(lcol):
+                if grid[i][j]!=0:
+                    tmp =4
+                    if i -1 >=0 and grid[i-1][j] : tmp -=1
+                    if i+1 <= lrow-1 and grid[i+1][j]: tmp-=1
+                    if j -1 >=0 and grid[i][j-1] : tmp -=1
+                    if j+1 <= lcol-1 and grid[i][j+1]: tmp-=1
+                    ret += tmp
+        return ret
